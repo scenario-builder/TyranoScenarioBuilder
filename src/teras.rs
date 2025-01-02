46bade5cf463fd4e6fa2d8fe146d2b6bed744795
darallium <contact@darallium.com>
2025-01-02 10:29:09 +0900

    さくっとログインだけ

---
+++ src/teras.rs
@@ -0,0 +1,6 @@
+use std::collections::{HashMap, BTreeMap};
+use tera::{Tera, Value, Error, from_value, to_value, Function};
+
+// Teraのfunctionsを登録するヘルパー関数
+pub fn register_tera_functions(tera: &mut Tera) {
+}
