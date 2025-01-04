3bbac94238148b59cdb954fcea2c02897e24cd86
gohan5858 <88976739+gohan5858@users.noreply.github.com>
2025-01-05 01:18:28 +0900

    chore: format & lint

--- src/views/login.rs
+++ src/views/login.rs
@@ -1,44 +1,42 @@
-use rocket::serde::Serialize;
-use rocket_dyn_templates::{Template, };
-use rocket::{get, post};
+use rocket::form::Form;
 use rocket::request::FlashMessage;
-use rocket::response::{Redirect, Flash};
-use rocket::http::{Status, Cookie};
-use rocket::form::{Form};
-
-use tera::{Context};
+use rocket::response::{Flash, Redirect};
+use rocket::serde::Serialize;
+use rocket::{get, post};
+use rocket_dyn_templates::Template;
 
 pub fn routes() -> Vec<rocket::Route> {
-  routes![login, login_post]
+    routes![login, login_post]
 }
 
 #[derive(Serialize, FromForm)]
 #[serde(crate = "rocket::serde")]
 pub struct LoginForm {
-  username: String,
-  password: String,
-  flash: Option<(String, String)>,
+    username: String,
+    password: String,
+    flash: Option<(String, String)>,
 }
 
 #[get("/")]
 pub fn login(flash: Option<FlashMessage<'_>>) -> Template {
-  let flash = flash.map(FlashMessage::into_inner);
-  let context = LoginForm {
-    username: "".into(),
-    password: "".into(),
-    flash: flash
-  };
-  Template::render("user/login", &context)
+    let flash = flash.map(FlashMessage::into_inner);
+    let context = LoginForm {
+        username: "".into(),
+        password: "".into(),
+        flash,
+    };
+    Template::render("user/login", &context)
 }
 
 #[post("/login", data = "<form>")]
-pub fn login_post(
-  form: Form<LoginForm>,
-  ) -> Result<Redirect, Flash<Redirect>> {
-  let form = form.into_inner();
-  if form.username == "admin" && form.password == "darallium" {
-    Ok(Redirect::to("/"))
-  } else {
-    Err(Flash::error(Redirect::to("/login"), "Invalid username or password"))
-  }
+pub fn login_post(form: Form<LoginForm>) -> Result<Redirect, Box<Flash<Redirect>>> {
+    let form = form.into_inner();
+    if form.username == "admin" && form.password == "darallium" {
+        Ok(Redirect::to("/"))
+    } else {
+        Err(Box::new(Flash::error(
+            Redirect::to("/login"),
+            "Invalid username or password",
+        )))
+    }
 }
--- src/views/project.rs
+++ src/views/project.rs
@@ -1,20 +1,14 @@
-use rocket::serde::Serialize;
-use rocket_dyn_templates::{Template, };
-use rocket::{get, post};
-use rocket::request::FlashMessage;
-use rocket::response::{Redirect, Flash};
-use rocket::http::{Status, Cookie};
-use rocket::form::{Form};
+use rocket::get;
+use rocket_dyn_templates::Template;
 
-use tera::{Context};
+use tera::Context;
 
 pub fn routes() -> Vec<rocket::Route> {
-  routes![scenarios]
+    routes![scenarios]
 }
-
 
 #[get("/scenarios")]
 fn scenarios() -> Template {
-  let mut ctx = Context::new();
-  Template::render("scenario/project", &ctx.into_json())
+    let ctx = Context::new();
+    Template::render("scenario/project", ctx.into_json())
 }
