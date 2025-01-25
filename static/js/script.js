3fbb8a48bcf1e3a90ee3ea4eab941e7e6a5b0ae7
YoujinTea <takeanhime0126@gmail.com>
2025-01-26 03:47:05 +0900

    Blockly動かせるかのテスト2: 文字列出力

--- static/js/script.js
+++ static/js/script.js
@@ -8,27 +8,11 @@ if(sidebarBtn && sidebar) {
   })
 }
 
-console.log(Blockly);
-
 const a_block = {
   init: function () {
     this.appendEndRowInput('NAME')
       .appendField('a')
-      .appendField(new Blockly.FieldTextInput('default'), 'b');
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
-    this.setTooltip('');
-    this.setHelpUrl('');
-    this.setColour(225);
-  }
-};
-
-const b_block = {
-  init: function () {
-    this.appendEndRowInput('NAME')
-      .appendField('b')
-      .appendField(new Blockly.FieldTextInput('default'), 'b')
-      .appendField('b');
+      .appendField(new Blockly.FieldTextInput('default'), 'input');
     this.setPreviousStatement(true, null);
     this.setNextStatement(true, null);
     this.setTooltip('');
@@ -39,5 +23,47 @@ const b_block = {
 
 Blockly.common.defineBlocks({ a_block: a_block });
 
+javascript.javascriptGenerator.forBlock['a_block'] = function (block) {
+  const text_input = block.getFieldValue('input');
+  const code = `a(${text_input})`;
+  return code;
+}
+
+const b_block = {
+  init: function () {
+    this.appendEndRowInput('NAME')
+      .appendField('b')
+      .appendField(new Blockly.FieldTextInput('default'), 'input')
+      .appendField('b');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('');
+    this.setHelpUrl('');
+    this.setColour(225);
+  }
+};
+
 Blockly.common.defineBlocks({ b_block: b_block });
 
+javascript.javascriptGenerator.forBlock['b_block'] = function (block) {
+  const input_text = block.getFieldValue('input');
+  const code = `b(${input_text})`;
+  return code;
+}
+
+const start_block = {
+  init: function () {
+    this.appendEndRowInput('NAME')
+      .appendField('start');
+    this.setNextStatement(true, null);
+    this.setTooltip('');
+    this.setHelpUrl('');
+    this.setColour(225);
+  }
+};
+
+Blockly.common.defineBlocks({ start_block: start_block });
+
+javascript.javascriptGenerator.forBlock['start_block'] = function () {
+  return "";
+}
