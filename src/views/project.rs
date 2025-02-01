be5ab0713b4498150d1aa3d0c74373b29ae4b7f0
darallium <contact@darallium.com>
2025-02-02 02:41:44 +0900

    エンドポイント誕生

--- src/views/project.rs
+++ src/views/project.rs
@@ -4,11 +4,17 @@ use rocket_dyn_templates::Template;
 use tera::Context;
 
 pub fn routes() -> Vec<rocket::Route> {
-    routes![scenarios]
+    routes![scenarios, scenario_edit]
 }
 
 #[get("/scenarios")]
 fn scenarios() -> Template {
     let ctx = Context::new();
     Template::render("scenario/project", ctx.into_json())
+}
+
+#[get("/scenario/<scenario_id>/edit")]
+fn scenario_edit() -> Template {
+    let ctx = Context::new();
+    Template::render("scenario/edit", ctx.into_json())
 }
